b7d5ede478becc32014b6ed61eba98c308398346
Vasileios <vtvasileiadis@gmail.com>
2024-06-19 19:42:50 +0300

    create user repository

---
+++ src/main/java/com/techpro/project/bookcatalog/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.techpro.project.bookcatalog.repository;
+
+import com.techpro.project.bookcatalog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findByUsername(String username);
+}
