5292334df9166170f1b0c7d64f35a394fb8e855e
Vasileios <vtvasileiadis@gmail.com>
2024-06-27 12:05:01 +0300

    exclude securityautoconfig

--- src/main/java/com/techpro/project/bookcatalog/BookcatalogApplication.java
+++ src/main/java/com/techpro/project/bookcatalog/BookcatalogApplication.java
@@ -2,11 +2,11 @@ package com.techpro.project.bookcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BookCatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookCatalogApplication.class, args);
 	}
-
 }
